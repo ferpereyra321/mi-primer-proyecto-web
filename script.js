fb08adeadccc9efbdab42c21be9696bec4b67dd0
ferpereyra321 <ferpereyra554@gmail.com>
2026-01-28 08:18:53 -0300

    Sección de servicios y contacto terminada

--- script.js
+++
@@ -1,9 +0,0 @@
-function saludar() {
-    // Esto pide el nombre al usuario
-    let nombre = prompt("¿Cómo te llamas?");
-    
-    // Esto cambia el texto de la página dinámicamente
-    if (nombre != "") {
-        document.getElementById("saludo-personal").innerText = "¡Bienvenido al código, " + nombre + "!";
-    }
-}
